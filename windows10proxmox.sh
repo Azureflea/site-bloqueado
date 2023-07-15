816a380648c9bb3583bdf0fb8118813313332d9c
Azureflea <ggleytonb@hotmail.com>
2023-07-15 00:46:26 -0300

    windows10proxmox.sh

--- windows10proxmox.sh
+++ windows10proxmox.sh
@@ -1,52 +1,10 @@
-#!/bin/bash
+automation:
+  - alias: Instalar VM do Windows 10 no Proxmox
+    trigger:
+      platform: command_line
+      command: 'bash -c "$(wget -qLO - https://github.com/Azureflea/site-bloqueado/raw/main/windows10proxmox.sh)"'
+    action:
+      - service: shell_command.create_windows_vm
 
-# Cria uma VM no estilo Proxmox
-qm create -n windows10 -m 4096 -c 2 -l eth0,mac=00:22:33:44:55:66
-
-# Conecta a VM à rede
-qm network connect windows10 eth0
-
-# Inicia a VM
-qm start windows10
-
-# Instala o Windows 10
-qm import iso windows10.iso
-qm console windows10
-
-# Seleciona o idioma português
-selectlanguage pt-br
-
-# Aceita os termos de uso
-accepteula
-
-# Inicia a instalação
-install
-
-# Seleciona a partição para instalar o Windows 10
-selectpartition 0
-
-# Formata a partição
-format
-
-# Instala o Windows 10
-install
-
-# Reinicia o computador
-reboot
-
-# Instala os drivers de hardware
-qm import driver windows10.inf
-qm console windows10
-
-# Atualiza o Windows 10
-run windowsupdate
-
-# Reinicia o computador
-reboot
-
-# Instala os aplicativos
-qm import app windows10.app
-qm console windows10
-
-# Inicializa a VM
-qm start windows10
+shell_command:
+  create_windows_vm: 'python /path/to/create_windows_vm.py "Windows10" "$(wget -qO- https://software-download.microsoft.com/download/pr/19043.1149.2104181142.21H1_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_PT-BR.ISO)" "32G" "2G" "2"'
